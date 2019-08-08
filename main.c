2f399bd18f37dff4113d6070661e01339d3f3e50
marijam19 <markovic.n.maja@gmail.com>
2019-08-08 22:36:54 +0200

    Stavljena tekstura

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include<math.h>
 #include<stdarg.h>
 #include<GL/glut.h>
+#include <GL/freeglut.h>
+#include"./SOIL.h"
 
   /* Deklaracije callback funkcija. */
 void on_keyboard(unsigned char key, int x, int y);
@@ -12,22 +14,31 @@ void on_display(void);
 void on_timer(int id);
 void material_and_light();
 void generate_polygon();
+void define_textures();
+void dealloc();
 
   /* Parametar animacije, fleg koji odredjuje da li je animacija u toku ili nije */
 #define TIMER_ID 0
 #define TIMER_INTERVAL 10
 
-  /*Parametar za kretanje kvadra.*/
+/*Parametar za kretanje kvadra.*/
 float animation_parameter = 0;
 int animation_ongoing = 0;
 
-int n, m, p, q, indikator=0, up_key=0, key_pre=0, ind_pre=0;
-int **mat;
-float curr_i=7, curr_j=3;
+/*Parametri za pomeranje kamere.*/
+float cam1=0, cam2=0;
+
+int n, m, p, q, indikator=0, up_key=0, key_pre=0, ind_pre=0, count_3=0, count_4=0, 
+    count_1=0, count_2=0, level=0;
+static int **mat;
+float curr_i=8, curr_j=2;
 float rot=0;
+float r_x=0, r_z=0, r_y=0;
 float lx=0, lz=-1;
 float x=0, z=5; 
 float angle;
+
+GLuint snow, end;
 
 int main(int argc, char **argv)
 {
@@ -41,10 +52,12 @@ int main(int argc, char **argv)
     glutCreateWindow(argv[0]);
  
     /* Poziv callback funkcija. */
-    glutDisplayFunc(on_display);
     glutReshapeFunc(on_reshape);
+    glutDisplayFunc(on_display);
     glutKeyboardFunc(on_keyboard);
     glutSpecialFunc(on_keyboards);
+ 
+    //glutTimerFunc(TIMER_INTERVAL, on_timer, TIMER_ID);
     
     /* OpenGL inicijalizacija. */
     glClearColor(0.95, 0.4, 0.2, 0);
@@ -55,15 +68,13 @@ int main(int argc, char **argv)
 
     glEnable(GL_NORMALIZE);
     
+    define_textures();
+    
     generate_polygon();
     
     /* Program ulazi u glavnu petlju. */
     glutMainLoop();
     
-   for(int i=0; i<n; i++)
-    free(mat[i]);
-
-  free(mat);
     return 0;
 }
  
@@ -71,36 +82,50 @@ void on_keyboards(int key, int x, int y){
  switch (key){
   case GLUT_KEY_LEFT:
       if(animation_ongoing){
+          
+          count_3++;
+          count_1=0;
+          count_2=0;
+          count_4=0;
           key_pre=up_key;
           ind_pre=indikator;
           up_key=3;
-          if(indikator==0)   //uspravno
-              indikator=1;   //vodoravno
+          if(indikator==0){   //uspravno
+              indikator=1;
+          }//vodoravno
           else if(indikator==1 && ind_pre==0){
               if(up_key==1 || up_key==2){
                 indikator=0;
                 curr_j--;
+                count_3=0;
               }
               else{
                 indikator=1;
             }
           }
            else if(indikator==1 && ind_pre==1){
-               if(key_pre==3){
+               //if(count_3>1)
+               count_3--;
+               if((key_pre==4 || key_pre==3) && count_3==1){
                    indikator=0;
                    curr_j--;
+                   count_3=0;
                }
                else{
                    indikator=1;
                }
            }
           curr_j-=1;
-          printf("ind:%d, i:%.1f, j:%.1f, up_key:%d, b_key:%d, ind_pre: %d\n", indikator, curr_i, curr_j, up_key, key_pre, ind_pre);
+    
         glutPostRedisplay();
     }
     break;
    case GLUT_KEY_RIGHT:
           if(animation_ongoing){
+              count_1=0;
+              count_2=0;
+              count_3=0;
+              count_4++;
           key_pre=up_key;
           ind_pre=indikator;
           up_key=4;        
@@ -109,67 +134,94 @@ void on_keyboards(int key, int x, int y){
               curr_j++;
           }
           else if(indikator==1 && ind_pre==0){
-              if(up_key==1 || up_key==2)
+              if(up_key==1 || up_key==2){
                 indikator=0;
-              else
+                count_4=0;
+              }
+              else{
                 indikator=1;
+              }
           }
           else if(indikator==1 && ind_pre==1){
-            if(key_pre==up_key || key_pre==1 || key_pre==2  || key_pre==3)
+              count_4--;
+            if((key_pre==3 || key_pre==4) && count_4==1){
                 indikator=0;
-            else
+            count_4=0;
+            }
+            else{
                 indikator=1;
+            }
           }
           curr_j+=1;
-                    printf("ind:%d, i:%.1f, j:%.1f, up_key:%d, b_key:%d, ind_pre: %d\n", indikator, curr_i, curr_j, up_key, key_pre, ind_pre);
           glutPostRedisplay();
     }
     break;
  case GLUT_KEY_UP:
           if(animation_ongoing){
+              count_2=0;
+              count_3=0;
+              count_4=0;
+              count_1++;
           key_pre=up_key;
           ind_pre=indikator;
               up_key=1;
           if(indikator==0){
               indikator=1;
+              printf("5\n");
           }
           else if(indikator==1 && ind_pre==0){
               indikator=0;
-              curr_i--;
+            
           }
           else if(indikator==1 && ind_pre==1){
-              if(key_pre==up_key || key_pre==2 || key_pre==3  || key_pre==4){
+              if(count_1>1)
+              count_1--;
+              if((key_pre==up_key || key_pre==2 || key_pre==3  || key_pre==4) && count_1==1){
                    indikator=0;
                    curr_i--;
+                   if(count_1!=0)
+                    count_1=0;
               }
-               else
+               else{
               indikator=1;
+               printf("3\n");
+               }
           }
           curr_i-=1;          
-          printf("ind:%d, i:%.1f, j:%.1f, up_key:%d, b_key:%d, ind_pre: %d\n", indikator, curr_i, curr_j, up_key, key_pre, ind_pre);
           glutPostRedisplay();
     }
     break;
  case GLUT_KEY_DOWN:
           if(animation_ongoing){
+              count_1=0;
+              count_3=0;
+              count_4=0;
+              count_2++;
           key_pre=up_key;
           ind_pre=indikator;
               up_key=2;
           if(indikator==0){
               indikator=1;
               curr_i++;
+ printf("4\n");
           }
           else if(indikator==1 && ind_pre==0){
               indikator=0;
           }
           else if(indikator==1 && ind_pre==1){
-              if(key_pre==1 || key_pre==up_key || key_pre==3 || key_pre==4)
+              if(count_2>1)
+              count_2--;
+              if((key_pre==1 || key_pre==up_key || key_pre==3 || key_pre==4) && count_2==1){
                 indikator=0;
-              else
+                if(count_2!=0)
+                 count_2=0;
+              }
+              else{
                 indikator=1;
+                curr_i++;  
+            }
           }
           curr_i+=1;
-                    printf("ind:%d, i:%.1f, j:%.1f, up_key:%d, b_key:%d, ind_pre: %d\n", indikator, curr_i, curr_j, up_key, key_pre, ind_pre);
           glutPostRedisplay();
     }
     break;
@@ -180,6 +232,7 @@ void on_keyboard(unsigned char key, int x, int y)
 {
     float c = 0.1;
    
+    
     switch (key) {
     case 27:  /* Zavrsava se program. */
       exit(0);
@@ -190,9 +243,10 @@ void on_keyboard(unsigned char key, int x, int y)
             animation_ongoing = 1;
             glutTimerFunc(TIMER_INTERVAL, on_timer, TIMER_ID);
         }
-        break;
+        break;                    
     } 
 }
+ 
 
     /*    sajt odakle je preuzeta nijansa plave boje
         https://community.khronos.org/t/color-tables/22518
@@ -202,7 +256,18 @@ void on_keyboard(unsigned char key, int x, int y)
 void generate_polygon(){
 
  FILE *dat;
- dat = fopen("poligon.txt", "r");
+ if(level==0){
+    dat = fopen("lvl1.txt", "r");
+ }
+ else if(level==1){
+    dat = fopen("lvl2.txt", "r");
+    curr_i=7;
+    curr_j=3;
+ }
+ else if(level==2){
+     dat = fopen("lvl3.txt", "r");
+ }
+
  if(dat==NULL)
   exit(0);
  fscanf(dat, "%d%d", &n, &m);
@@ -237,11 +302,12 @@ void on_display(void)
         material_and_light();
     
     gluLookAt(
-            8, 10, 12,
+            8, 8, 14,
             x+lx, 1, z+lz,
             0, 1, 0
         );
     
+    if(level<=2){
         glBegin(GL_LINES);
      /*X-red*/
             glColor3f(1,0,0);
@@ -256,9 +322,38 @@ void on_display(void)
             glVertex3f(0, 0, 0);
             glVertex3f(0, 0, 100);
         glEnd();
+
+        /* Omogucavamo teksture */
+        glEnable(GL_TEXTURE_2D);
+    
         
- int ind=0;
-    /*0-padanje, 1-kocka, 4-cilj*/
+       glTranslatef(10, -9, -10);
+        glRotatef(-40, 0,1,0);
+        glRotatef(40, 1,0,0);
+        glBindTexture(GL_TEXTURE_2D, snow);
+        glBegin(GL_QUADS);
+            glNormal3f(1, -1, 1);
+
+            glTexCoord2f(0, 0);
+            glVertex3f(-400.0, 0, 400.0);
+
+            glTexCoord2f(4, 0);
+            glVertex3f(400.0, 0, 400.0);
+
+            glTexCoord2f(4, 4);
+            glVertex3f(400.0, 0, -400.0);
+
+            glTexCoord2f(0, 4);
+            glVertex3f(-400.0, 0, -400.0);
+        glEnd();
+        glRotatef(-40, 1,0,0);
+        glRotatef(40, 0,1,0);
+        glTranslatef(-10, 9, 10);
+        glDisable(GL_TEXTURE_2D);
+        glEnable(GL_COLOR_MATERIAL);
+     int ind=0;   
+   
+     /*0-padanje, 1-kocka, 4-cilj*/
  for(int i=0; i<n; i++){
    for(int j=0; j<m; j++){
        ind++;
@@ -278,19 +373,26 @@ void on_display(void)
     	glColor3f(0, 0, 0);
        glutSolidCube(1);
       glPopMatrix();
+      if(curr_i==i && curr_j==j){
+           printf("SUCCESS!!!\n");
+           level++;
+           dealloc();
+           // exit(0);
+           generate_polygon();
+      }
     }
     else if(mat[i][j]==0){
-     /*   if(curr_i==i && curr_j==j){
+      /*  if(curr_i==i && curr_j==j){
             printf("padanje");
-            exit(0);
+            exit(0);*/
+      continue;
     }
-    */
-    }
+    
     else 
       continue;
    }
  }
-    
+ 
  /*uspravan kvadar*/
     if(indikator==0){
         if(up_key==1 || up_key==2){
@@ -311,19 +413,24 @@ void on_display(void)
       }
     }
     else if(indikator==1){
-         if(
-             (ind_pre==1 && up_key==4) || (ind_pre==1 && up_key==3 && (key_pre==1 || key_pre==2 || key_pre==3)) || 
+        if((up_key==1 || up_key==2) && ind_pre==1 && (key_pre==3 || key_pre==4)){
+            glPushMatrix();
+            glColor3f(1, 0, 0);
+            glTranslatef(curr_j-0.5, 1, curr_i);
+            glScalef(2, 1, 1);
+            glutSolidCube(1);
+            glPopMatrix();
+        }
+        else if((ind_pre==1 && up_key==4) || (ind_pre==1 && up_key==3) || 
              (indikator==1 && up_key==1) || (indikator==1 && up_key==2)){
             glPushMatrix();
-            glColor3f(0, 1, 0);
+            glColor3f(1, 0, 0);
             glTranslatef(curr_j, 1, curr_i-0.5);
             glScalef(1, 1, 2);
             glutSolidCube(1);
             glPopMatrix();
          }
-         else if(((key_pre==1 || key_pre==2 || key_pre==4) && (ind_pre==0) && (up_key==4 || up_key==3)) ||
-             (ind_pre==1 && up_key==1) || (ind_pre==1 && up_key==2) || (indikator==1 && up_key==4) || 
-             (indikator==1 && up_key==3)){
+         else{ //vodoravno po x
             glPushMatrix();
             glColor3f(1, 0, 0);
             glTranslatef(curr_j-0.5, 1, curr_i);
@@ -331,17 +438,37 @@ void on_display(void)
             glutSolidCube(1);
             glPopMatrix();
         }
-         else{ //vodoravno po x 
-            glPushMatrix();
-            glColor3f(1, 0, 0);
-            glTranslatef(curr_j-0.5, 1, curr_i);
-            glScalef(2, 1, 1);
-            glutSolidCube(1);
-            glPopMatrix();
-        }
-        
     }
 
+    
+    }
+    else{  //kraj
+         glLoadIdentity();
+        gluLookAt(-1, 0, 0, 
+                   0, 0, 0, 
+                   0, 1, 0);
+        
+//         glTranslatef(0,-50,0);
+//         glScalef(0.8,1,1.5);
+//         glEnable(GL_TEXTURE_2D);
+//         glBindTexture(GL_TEXTURE_2D, end);
+//         glBegin(GL_QUADS);
+//             glNormal3f(1, -1, 1);
+// 
+//             glTexCoord2f(0, 0);
+//             glVertex3f(400, 0, -400);
+//  
+//             glTexCoord2f(1, 0);
+//             glVertex3f(400, 0, 400);
+//    
+//             glTexCoord2f(1, 1);
+// 
+//             glTexCoord2f(0, 1);
+//             glVertex3f(400, 100, -400);
+//         glEnd();
+//         glDisable(GL_TEXTURE_2D);
+//         glEnable(GL_COLOR_MATERIAL);
+    }
     /* Nova slika se salje na ekran. */
     glutSwapBuffers();
 }
@@ -368,6 +495,14 @@ void on_reshape(int width, int height)
     gluPerspective(70, (float)width / height, 1, 100);
 }
 
+
+void dealloc(){
+for(int i=0; i<n; i++)
+    free(mat[i]);
+
+  free(mat);
+}
+
 void material_and_light(){
      /* Parametri svetla i podrazumevanog materijala */
     GLfloat light_ambient[] = { 0.1, 0.1, 0.1, 1 };
@@ -390,3 +525,32 @@ void material_and_light(){
     glMaterialfv(GL_FRONT, GL_SPECULAR, specular_coeffs);
     glMaterialf(GL_FRONT, GL_SHININESS, shininess);
 }
+
+void define_textures(){
+    
+      /* Inicijalizuje se tekstura */
+    glEnable(GL_TEXTURE_2D);
+    glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+    
+    /* Pozadina */
+    glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+
+        snow = SOIL_load_OGL_texture("./snow.png", 
+                                SOIL_LOAD_AUTO, 
+                                SOIL_CREATE_NEW_ID, 
+                                SOIL_FLAG_INVERT_Y);
+    /* U slucaju da ucitavanje strukture nije uspelo, ispisati vrstu greske i prekinuti program */
+     if(snow == 0)
+         exit(0);
+     glBindTexture(GL_TEXTURE_2D, snow);
+     glTexParameteri(GL_TEXTURE_2D,
+                     GL_TEXTURE_WRAP_S, GL_REPEAT);
+     glTexParameteri(GL_TEXTURE_2D,
+                     GL_TEXTURE_WRAP_T, GL_REPEAT);
+     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+    /* Iskljucujemo aktivnu teksturu */
+    glBindTexture(GL_TEXTURE_2D, 0);
+}
+
